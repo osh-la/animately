6e06e52ddc7015cd14b08047226d5dab500665d4
abimiku oshela <abimikuoshela@gmail.com>
2025-01-06 17:08:56 +0100

    setup

---
+++ animitely/src/App.tsx
@@ -0,0 +1,20 @@
+import {Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+import MainLayout from './Layouts/mainLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+
+    </Route>
+  )
+)
+const App=()=> {
+  return <RouterProvider router={router} />
+    
+}
+
+export default App
