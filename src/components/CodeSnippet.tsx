ffd0143de950f8357597baf8dc1afd7db3d6d89a
abimiku oshela <abimikuoshela@gmail.com>
2025-03-30 13:48:04 +0100

    fix

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -42,7 +42,7 @@ const CodeSnippet: React.FC<CodeSnippetProps> = ({ animation }) => {
 
      <div className="flex justify-center">
      <button className="mt-4 text-sm md:text-base bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition">
-        Generate Tailwind na zuwa...
+        Generate Tailwind coming soon...
       </button>
      </div>
     </div>
